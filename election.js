69f766bc7a855c08ef09af3f99e11486df9a8646
Anthematics <jasonharder2@gmail.com>
2017-11-06 17:19:34 -0500

    successful ajax request written and logged to console

--- election.js
+++ election.js
@@ -1,5 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  // Imagination!
+//In the election.js file, build an AJAX request that makes a GET request to the root path,
+//and expects a JSON response.
+	$.ajax ({
+	url:"https://bb-election-api.herokuapp.com/",
+	method:"get",
+	datatype:"json"
 
+}).done(response => console.log(response.candidates)) 
 });
+//Make sure you're making this request within your DOMContentLoaded block
+//Add a <ul> to your index.html file, with an id. This is what we'll attach our data to.
+//In the .done(function(responseData){}) callback function for the AJAX request loop over the candidates in responseData, and append a <li> element for each candidate into the DOM at our <ul> from the last step. You'll want to show the name and votes count of each candidate.  // Imagination!
